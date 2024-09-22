7aa293e960d588212675051f804b9921e1f0acc4
Bruno Sabot <brunosabot@gmail.com>
2024-09-22 10:18:25 +0200

    fix: missing replacement verificfation with primitives

--- src/evaluateVariables-helper.js
+++ src/evaluateVariables-helper.js
@@ -2,7 +2,10 @@ import formatVariables from "./formatVariables-helper";
 
 export const replaceWithKeyValue = (stringTemplate, key, value) => {
   if (typeof value === "number" || typeof value === "boolean") {
-    return stringTemplate.replaceAll(`"[[${key}]]"`, value);
+    return stringTemplate
+      .replaceAll(`'[[${key}]]'`, value)
+      .replaceAll(`"[[${key}]]"`, value)
+      .replaceAll(`\`[[${key}]]\``, value);
   } else if (typeof value === "object") {
     return stringTemplate
       .replaceAll(`"[[${key}]]"`, JSON.stringify(value))
